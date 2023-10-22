833a5ad4eb217a715f4675ce7836cceea2f37f4c
Saif <ayeshsaif367@gmail.com>
2023-10-22 06:37:26 +0300

    modify api baseURL

--- src/api.ts
+++ src/api.ts
@@ -6,6 +6,6 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/projects/ecommerce/'
+  baseURL: 'https://saifchan.online/projects/ecommerce/'
 })
 export {api};
